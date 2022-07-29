1e795b013f9c6556c64a8a7fd0363d1bfb3440e6
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2022-07-29 10:42:04 +0200

    Add materialization + ojects as props

--- src/components/listParentChildren.tsx
+++ src/components/listParentChildren.tsx
@@ -1,32 +1,41 @@
 import { Action, ActionPanel, Icon, List } from "@raycast/api";
+import { dbtEnv, dbtModelShort } from "../types";
 import { ModelDetails } from "./modelDetails";
 
 export function ListParentChildren(props: {
-  envId: string;
-  children: Array<string>;
-  parents: Array<string>;
+  env: dbtEnv;
+  children: Array<dbtModelShort>;
+  parents: Array<dbtModelShort>;
   model: string;
+  listModels: Array<dbtModelShort>;
   jobIdNameMapping: Record<string, string>;
+  directParentsUniqueId: Array<string>;
 }) {
-  const envId = props.envId;
+  const env = props.env;
   const children = props.children;
   const parents = props.parents;
   const model = props.model;
+  const listModels = props.listModels;
   const jobIdNameMapping = props.jobIdNameMapping;
+  const directParentsUniqueId = props.directParentsUniqueId;
 
   return (
     <List navigationTitle={`Dependencies of ${model}`}>
       <List.Section title="Direct and Indirect Parents">
         {parents.map((node) => (
           <List.Item
-            key={node}
-            title={node.split(".")[2]}
-            accessories={[{ text: node.split(".")[1], icon: Icon.Hammer }]}
+            key={node.uniqueId}
+            title={node.name}
+            subtitle={node.materializedType}
+            // subtitle={(node.materializedType + " - " ) + directParentsUniqueId.includes(node.uniqueId) ? "Direct Parent" : "Indirect Parent"}
+            accessories={[{ text: node.packageName, icon: Icon.Hammer }]}
             actions={
               <ActionPanel>
                 <Action.Push
                   title="Show Details"
-                  target={<ModelDetails envId={envId} uniqueId={node} jobIdNameMapping={jobIdNameMapping} />}
+                  target={
+                    <ModelDetails env={env} model={node} jobIdNameMapping={jobIdNameMapping} listModels={listModels} />
+                  }
                 />
               </ActionPanel>
             }
@@ -36,14 +45,17 @@ export function ListParentChildren(props: {
       <List.Section title="Direct Children">
         {children.map((node) => (
           <List.Item
-            key={node}
-            title={node.split(".")[2]}
-            accessories={[{ text: node.split(".")[1], icon: Icon.Hammer }]}
+            key={node.uniqueId}
+            title={node.name}
+            subtitle={node.materializedType}
+            accessories={[{ text: node.packageName, icon: Icon.Hammer }]}
             actions={
               <ActionPanel>
                 <Action.Push
                   title="Show Details"
-                  target={<ModelDetails envId={envId} uniqueId={node} jobIdNameMapping={jobIdNameMapping} />}
+                  target={
+                    <ModelDetails env={env} model={node} jobIdNameMapping={jobIdNameMapping} listModels={listModels} />
+                  }
                 />
               </ActionPanel>
             }
